91c8c978a8f1fec345f8622a11b0dad7128da24a
Jabez <254613R@mymail.nyp.edu.sg>
2026-06-30 14:40:38 +0800

    Add files via upload

---
+++ ArcaneVault/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ArcaneVault.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
